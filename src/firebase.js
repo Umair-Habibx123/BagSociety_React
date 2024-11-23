142e40431c7c4ba8e831f3d481954dd18d325f53
Umair Habib <umairhabibx123@gmail.com>
2024-11-23 14:54:06 +0500

    initial commit3

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,25 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCTqY0RX26Caz5rfiemgQayqkeimFPA3HU",
+//   authDomain: "bagsociety.firebaseapp.com",
+//   projectId: "bagsociety",
+//   storageBucket: "bagsociety.firebasestorage.app",
+//   messagingSenderId: "231216891724",
+//   appId: "1:231216891724:web:3f825ffe73f1a1cfadf1e9",
+//   measurementId: "G-K2NR4QQTZR",
+// };
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCTqY0RX26Caz5rfiemgQayqkeimFPA3HU",
-  authDomain: "bagsociety.firebaseapp.com",
-  projectId: "bagsociety",
-  storageBucket: "bagsociety.firebasestorage.app",
-  messagingSenderId: "231216891724",
-  appId: "1:231216891724:web:3f825ffe73f1a1cfadf1e9",
-  measurementId: "G-K2NR4QQTZR",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 
